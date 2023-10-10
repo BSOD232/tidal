417323dca0eb6c477393e5c9ff6cf015d4dff321
BSOD232 <92183387+BSOD232@users.noreply.github.com>
2023-10-10 11:46:16 -0400

    bad words filter added

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,4 +1,10 @@
 const Post = require('../models/post');
+const fs = require('fs');
+const path = require('path');
+
+// Load the list of bad words from the file into an array
+const badWordsFile = path.join(__dirname, 'offensive-words.txt');
+const badWordsList = fs.readFileSync(badWordsFile, 'utf8').split('\n');
 
 const post_index = (req, res) => {
   Post.find()
@@ -12,15 +18,36 @@ const post_index = (req, res) => {
 };
 
 const post_create_post = (req, res) => {
-  const post = new Post(req.body);
-  console.log(post)
-  post.save()
-    .then(result => {
-      res.redirect('/');
-    })
-    .catch(err => {
-      console.log(err);
-    });
+  // Check if any of the words in title or body match the bad words
+  const title = req.body.title;
+  const body = req.body.body;
+
+  if (containsBadWords(title) || containsBadWords(body)) {
+    // Handle the case where bad words are found (e.g., reject the post)
+    console.log('Bad words found in title or body');
+    res.redirect('/error'); // Redirect to an error page or handle appropriately
+  } else {
+    // If no bad words are found, proceed to save the post
+    const post = new Post(req.body);
+    console.log(post);
+    post.save()
+      .then(result => {
+        res.redirect('/');
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+};
+
+function containsBadWords(text) {
+  const words = text.toLowerCase().split(/\s+/);
+  for (const word of words) {
+    if (badWordsList.includes(word)) {
+      return true;
+    }
+  }
+  return false;
 }
 
 // const blog_delete = (req, res) => {
